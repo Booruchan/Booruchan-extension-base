642d1c1609247f407e4954b4849c4e27abf47b81
Maxim_Khvostov <mkliverout@gmail.com>
2023-07-21 22:57:38 +0300

    feature: implement basic source interface

---
+++ src/main/kotlin/com/makentoshe/booruchan/extension/base/Source.kt
@@ -0,0 +1,12 @@
+package com.makentoshe.booruchan.extension.base
+
+interface Source {
+
+    /** Should be unique for each source */
+    val id: String
+
+    /** Title how it will be displayed to user */
+    val title: String
+
+}
+
